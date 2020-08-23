592d21139c074859a8960ae46ce9233dfb6ddc3d
ishantmishra <ishant.mishra.1029@gmail.com>
2020-08-23 19:13:17 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 56 % 5;
+        System.out.println(x);
+    }
+}
+ 
